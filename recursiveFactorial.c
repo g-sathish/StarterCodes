dbb1f372d2256ffb765bf4fcf5a45329ec121fa1
Sathish G <36729429+g-sathish@users.noreply.github.com>
2018-08-27 20:08:52 +0530

    Create recursiveFactorial.c
    Complete the recursive factorial function which returns the factorial of the number passed to it.

---
+++ recursiveFactorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//complete the function below
+int findFactorial(int number)
+{
+  
+}
+
+int main()
+{
+  int number;
+  scanf("%d",&number);
+  printf("%d",findFactorial(number));
+  return 0;
+}
